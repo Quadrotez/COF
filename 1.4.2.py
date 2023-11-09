09576306a47d42d7820253c227cbaf9f4c2fd61a
Kogotez <kogotezplay52245@gmail.com>
2023-11-09 23:09:16 +0300

    Create 1.4.2.py

---
+++ 1.4.2.py
@@ -0,0 +1,15 @@
+# cell – Width of cell in a symbols
+# cells – Number of cell in a string
+# row – Heght of the cell in a string
+# rows – Number of strings
+
+ 
+cell = int(input())
+cells = int(input())
+row = int(input())
+rows = int(input())
+ 
+ 
+print((('*' + ('-') * cell) * cells + '*\n') + ((('|' + (' ' * cell)) * cells + '|\n')) * row + (('*' + (cell*'-') + ('+' + '-' * cell) * (cells - 1) + '*\n') + (('|' + (' ' * cell)) * cells + '|\n') * row) * (rows - 1) + ('*' + ('-') * cell) + ('*' + ('-') * cell) * (cells - 1) + '*')
+
+input() # - This input is needed so that the program does not close 
